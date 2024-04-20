c7ebb007bbafd19b1639f2f9892b6a8d56fa7780
evermake <vladeryabkin@gmail.com>
2024-04-20 11:07:59 +0300

    initialize frontend with Nuxt

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="pb-[100px]">
+    <header>
+      <UContainer>Header</UContainer>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <UContainer>Footer</UContainer>
+    </footer>
+  </div>
+</template>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+function handleClick() {
+  colorMode.preference = colorMode.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-center">Hi there!</h1>
+  </div>
+</template>
