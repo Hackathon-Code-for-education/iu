f2296099e007476ac124f1ebf40710ed32bac66c
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-04-21 13:25:46 +0300

    fix: org name layout

--- frontend/components/OrganizationPage.vue
+++ frontend/components/OrganizationPage.vue
@@ -21,7 +21,7 @@ const { me } = useMe()
       <OrganizationPanorama v-if="mainSceneId" :org-username="orgUsername" :org-id="orgId" :main-scene-id="mainSceneId" />
       <div class="px-12 flex py-4 justify-between items-start">
         <div class="flex justify-between gap-5 items-end">
-          <div class="w-20 h-1 relative">
+          <div class="w-40 h-1 relative">
             <UAvatar
               class="absolute bottom-0 box-content border-4 border-neutral-900"
               size="3xl"
@@ -29,7 +29,7 @@ const { me } = useMe()
             />
           </div>
           <div>
-            <h1 class="text-2xl font-medium">
+            <h1 class="text-2xl font-medium line-clamp-1">
               {{ title }}
             </h1>
             <p class="text-neutral-500">
@@ -37,21 +37,19 @@ const { me } = useMe()
             </p>
           </div>
         </div>
-        <div class="flex flex-col gap-2 items-end">
-          <div class="flex justify-between gap-2">
-            <UButton @click="$emit('wantChatClick')">
-              Написать студентам
-            </UButton>
-            <UButton
-              v-if="me?.role === 'admin' || me?.role === 'moderator'"
-              variant="outline"
-              :to="`/${orgUsername}/edit/scenes`"
-              class="w-fit"
-              icon="i-mdi-pencil"
-            >
-              Редактировать
-            </UButton>
-          </div>
+        <div class="flex flex-row gap-2 items-end flex-shrink-0">
+          <UButton @click="$emit('wantChatClick')">
+            Написать студентам
+          </UButton>
+          <UButton
+            v-if="me?.role === 'admin' || me?.role === 'moderator'"
+            variant="outline"
+            :to="`/${orgUsername}/edit/scenes`"
+            class="w-fit"
+            icon="i-mdi-pencil"
+          >
+            Редактировать
+          </UButton>
         </div>
       </div>
     </Card>
