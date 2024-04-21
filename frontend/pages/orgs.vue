9ee248ef6489e43c0323a22124adfe5b3c04e958
evermake <vladeryabkin@gmail.com>
2024-04-21 11:11:10 +0300

    feat: orgs page improvements

--- frontend/pages/orgs.vue
+++ frontend/pages/orgs.vue
@@ -1,26 +1,27 @@
 <script lang="ts" setup>
 import { useOrganizationsReadAll } from '~/api'
+import { getFileUrl } from '~/api/file'
 
-const { data: orgs, isLoading } = useOrganizationsReadAll()
+const { data: orgs } = useOrganizationsReadAll()
 </script>
 
 <template>
   <UContainer>
     <div v-if="orgs" class="grid grid-cols-3 gap-4">
       <NuxtLink v-for="org in orgs.data" :key="org.id" :to="`/${org.username}`">
-        <Card class="px-4 h-[80px] flex items-center">
+        <Card class="px-4 h-[80px] flex gap-4 items-center hover:dark:border-green-700">
+          <UAvatar size="xl" :src="org.logo ? getFileUrl(org.logo) : undefined" />
           <h3 class="text-xl">
             {{ org.name }}
           </h3>
         </Card>
       </NuxtLink>
     </div>
-    <div v-else-if="isLoading" class="grid grid-cols-3 gap-4">
-      <USkeleton class="rounded-xl h-[80px] w-full" />
-      <USkeleton class="rounded-xl h-[80px] w-full" />
-      <USkeleton class="rounded-xl h-[80px] w-full" />
-      <USkeleton class="rounded-xl h-[80px] w-full" />
-      <USkeleton class="rounded-xl h-[80px] w-full" />
+    <div v-else class="grid grid-cols-3 gap-4">
+      <Card v-for="i in new Array(5).fill(null)" :key="i" class="px-4 h-[80px] flex gap-4 items-center">
+        <USkeleton class="h-14 w-14 rounded-full" />
+        <USkeleton class="rounded h-8 grow" />
+      </Card>
     </div>
   </UContainer>
 </template>
