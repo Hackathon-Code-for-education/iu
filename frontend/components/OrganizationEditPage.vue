f9ad407d283a501473f8440def6ac6ce020a52a7
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-04-21 12:29:56 +0300

    chore: add page for main info editing

---
+++ frontend/components/OrganizationEditPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useOrganizationsGetByUsername, useScenesGetScenesForOrganization } from '~/api'
+
+const props = defineProps<{
+  orgUsername: string
+  orgId: string
+}>()
+
+const { data: org } = useOrganizationsGetByUsername(props.orgUsername, { query: { retry: 0 } })
+const { data: scenes } = useScenesGetScenesForOrganization(props.orgId, { query: { retry: 0 } })
+</script>
+
+<template>
+  <UContainer v-if="org && scenes">
+    <div class="grid grid-cols-3 gap-4 mt-4">
+      <Card class="p-4 h-80" />
+      <Card class="p-4 h-80 col-span-2" />
+    </div>
+  </UContainer>
+</template>
---
+++ frontend/pages/[org]/edit/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { createError, useRoute } from '#app'
+import { useOrganizationsGetByUsername } from '~/api'
+
+const route = useRoute()
+const orgUsername = route.params.org as string
+
+const {
+  data: org,
+  error: queryError,
+  isLoading,
+} = useOrganizationsGetByUsername(orgUsername, { query: { retry: 0 } })
+
+const error = computed(() => {
+  const err = queryError.value
+  if (err && err.response?.status === 404) {
+    return createError({
+      statusCode: 404,
+      statusMessage: 'Организация не найдена',
+    })
+  }
+  return null
+})
+</script>
+
+<template>
+  <ErrorPage v-if="error" :error="error" />
+  <OrganizationEditHeader
+    v-if="org"
+    :org-username="orgUsername"
+    :org-id="org.data.id"
+  />
+  <OrganizationEditPage
+    v-if="org"
+    :org-username="orgUsername"
+    :org-id="org.data.id"
+  />
+  <UContainer v-else-if="isLoading">
+    <USkeleton class="w-full h-[300px] rounded-xl" />
+  </UContainer>
+</template>
