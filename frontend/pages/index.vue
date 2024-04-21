c32372ad1e4c2e1675dba2296451487a34b067b9
evermake <vladeryabkin@gmail.com>
2024-04-21 22:17:11 +0300

    feat: improve landing

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <h1 class="font-bold text-center text-3xl">
-      Добро пожаловать!
+  <div class="mt-12 flex flex-col justify-center items-center">
+    <h1 class="font-bold text-center text-[6rem]">
+      АбиТур
     </h1>
+    <h2 class="font-medium opacity-75 text-[1.5rem] text-center">
+      Платформа для ВУЗов, студентов и абитуриентов. Онлайн туры, честные отзывы, только правда.
+    </h2>
+    <UButton to="/orgs" class="mt-8" size="xl">
+      Выбрать ВУЗ мечты
+    </UButton>
   </div>
 </template>
