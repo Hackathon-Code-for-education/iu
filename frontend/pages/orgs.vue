09c2ba838314a80986a996d01182b94c0b765a0f
evermake <vladeryabkin@gmail.com>
2024-04-21 09:45:01 +0300

    feat: add organizations loading skeletons

--- frontend/pages/orgs.vue
+++ frontend/pages/orgs.vue
@@ -8,7 +8,7 @@ const { data: orgs, isLoading } = useOrganizationsReadAll()
   <UContainer>
     <div v-if="orgs" class="grid grid-cols-3 gap-4">
       <NuxtLink v-for="org in orgs.data" :key="org.id" :to="`/${org.username}`">
-        <Card class="p-6">
+        <Card class="px-4 h-[80px] flex items-center">
           <h3 class="text-xl">
             {{ org.name }}
           </h3>
@@ -16,7 +16,11 @@ const { data: orgs, isLoading } = useOrganizationsReadAll()
       </NuxtLink>
     </div>
     <div v-else-if="isLoading" class="grid grid-cols-3 gap-4">
-      <USkeleton class="rounded-xl" />
+      <USkeleton class="rounded-xl h-[80px] w-full" />
+      <USkeleton class="rounded-xl h-[80px] w-full" />
+      <USkeleton class="rounded-xl h-[80px] w-full" />
+      <USkeleton class="rounded-xl h-[80px] w-full" />
+      <USkeleton class="rounded-xl h-[80px] w-full" />
     </div>
   </UContainer>
 </template>
